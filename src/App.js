f21356bcc86558243b2c8a2e63cb03c5625c78a5
mustran <ademimusli000@gmail.com>
2019-08-01 01:30:49 +0200

    chatkit added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from "react";
+import Chatkit from "@pusher/chatkit-client";
+import { tokenUrl, instanceLocator } from "./config";
+class App extends Component {
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator,
+            userId: "musli",
+            tokenProvider: new Chatkit.TokenProvider({
+                url: tokenUrl
+            })
+        });
 
-function App() {
-  return (
-    <div className="App">
-      test push
-    </div>
-  );
+        chatManager.connect()
+        .then(currentUser => {
+            currentUser.subscribeToRoom({
+                roomId: "31264558",
+                hooks: {
+                    onMessage: message => {
+                        console.log("message.text " , message.text, currentUser.rooms);
+                    }
+                },
+                // messageLimit: 0
+            });
+            console.log("Connected as user ", currentUser)
+        });
+    }
+
+    render() {
+        return <div className="App">test push</div>;
+    }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const tokenUrl =
+    "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/772b566e-ebfe-440b-993d-a598cb66c166/token";
+
+const instanceLocator = "v1:us1:772b566e-ebfe-440b-993d-a598cb66c166";
+
+export { tokenUrl, instanceLocator };
